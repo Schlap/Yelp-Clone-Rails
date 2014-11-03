e867b13795e856008f94e32fe7f051b312362b8a
Hadi <hadichalabi@gmail.com>
2014-11-03 17:50:17 +0000

    added delete restaurant functionality

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -64,6 +64,19 @@ context 'editing restaurants' do
   end
 end
 
+describe 'deleting restaurants' do
+
+  before do
+    Restaurant.create(:name => "KFC")
+  end
+
+  it 'removes restaurant when a user clicks a delete link' do
+    visit '/restaurants'
+    click_link 'Delete KFC'
+    expect(page).not_to have_content 'KFC'
+    expect(page).to have_content 'Restaurant deleted successfully'
+  end
+end
 
 
 
