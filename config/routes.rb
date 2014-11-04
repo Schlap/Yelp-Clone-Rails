4dcf315eca86c5f85e31f68020d4d738fdbecf7e
Hadi <hadichalabi@gmail.com>
2014-11-04 14:26:39 +0000

    added facebook authentication api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy'
+  end
+
   root to: "restaurants#index"
   resources :restaurants do
     resources :reviews
