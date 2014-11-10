e7818a940928cbe41efa6cdcc3581841ae6303fd
Hadi <hadichalabi@gmail.com>
2014-11-10 13:22:51 +0000

    fixed defualt image setting

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/no_image.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/no_image.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def average_rating
@@ -10,4 +10,3 @@ class Restaurant < ActiveRecord::Base
     reviews.inject(0) {|memo, review| memo + review.rating} / reviews.count
   end
 end
-
